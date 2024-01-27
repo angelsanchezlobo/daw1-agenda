80e0ef7a0ce6177b32e6cd525690d2118302d4f0
Adam Castillo <admpkp24@gmail.com>
2024-01-27 14:53:32 +0100

    canvisAdam

---
+++ Agenda-ACastillo/Agenda-ACastillo/Program.cs
@@ -0,0 +1,177 @@
+﻿
+using System.Diagnostics;
+
+namespace Agenda_ACastillo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcioS = ""; int numero1, numero2, aux;
+            while (opcioS != "Q" && opcioS != "q")
+            {
+                Agenda();
+                Console.Write("Quina opció vols escollir? ");
+                opcioS = Console.ReadLine();
+                if (opcioS == "Q" || opcioS == "q")
+                {
+                    BorrarConsola();
+                    Console.WriteLine("Adeu!");
+                }
+                else if (!ValidarOpcio(opcioS))
+                {
+                    Console.WriteLine("No has escollit una opció de les possibles.");
+                    retorn();
+                    BorrarConsola();
+                }
+                else
+                {
+                    aux = Convert.ToInt32(opcioS);
+                    switch(aux)
+                    {
+                        //Veure llistat de persones de l'agenda
+                        case 1:
+                            BorrarConsola();
+                            Capcalera1();
+                            //Afegir el codi a partir d'aqui, no borrar retorn
+                            retorn();
+                            BorrarConsola();
+                            break;
+                        //Afegir contactes a l'agenda
+                        case 2:
+                            Capcalera2();
+                            //Afegir el codi a partir d'aqui, no borrar retorn
+                            retorn();
+                            BorrarConsola();
+                            break;
+                        //Esborrar contactes
+                        case 3:
+                            Capcalera3();
+                            //Afegir el codi a partir d'aqui, no borrar retorn
+                            retorn();
+                            BorrarConsola();
+                            break;
+                        //Llistat de contactes esborrats
+                        case 4:
+                            Capcalera4();
+                            //Afegir el codi a partir d'aqui, no borrar retorn
+                            retorn();
+                            BorrarConsola();
+                            break;
+                        default:
+                            Console.WriteLine("");
+                            break;
+                    }
+                }
+            }
+        }
+        static void Agenda()
+        {
+            ConsolaFons();
+            Capcalera();
+            MenuText();
+            return;
+        }
+        static bool ValidarOpcio(string opcioS) // Aquest validara si la opció que hem escollit esta entre els valors possibles.
+        {
+            int opcioN = 0;
+            bool validacio = false;
+            if ("4321".Contains(opcioS))
+            {
+                opcioN = Convert.ToInt32(opcioS);
+                validacio = true;
+                if (opcioN > 4 || opcioN < 1)
+                {
+                    validacio = false;
+                }
+            }
+            else
+            {
+                validacio = false;
+            }
+            return validacio;
+        }
+        static void retorn()
+        {
+            int contador = 5;
+            while (contador >= 0) 
+            {
+                Console.Write("\r");
+                Console.Write($"Tornant al menù principal en {contador} segons...");
+                Thread.Sleep(1000);
+                contador--;
+            }
+            return;
+        }
+        static void BorrarConsola() // Aquest borrara la consola jo vulgui tornant d'aquesta manera al principi
+        {
+            Console.Clear();
+            return;
+        }
+        static string MenuText() // Aquest es el menu en text
+        {
+            string text;
+            text = "╔═══════════════════════════════════════════════════════╗\n" +
+                   "║                        OPCIONS                        ║\n" +
+                   "╠═══════════════════════════════════════════════════════╣\n" +
+                   "║                                                       ║\n" +
+                   "║                  1 - VEURE LLISTAT                    ║\n" +
+                   "║                  2 - AFEGIR CONTACTES                 ║\n" +
+                   "║                  3 - ESBORRAR CONTACTES               ║\n" +
+                   "║                  4 - LLISTAT DE CONTACTES ESBORRADES  ║\n" +
+                   "║                  Q - SORTIR                           ║\n" +
+                   "║                                                       ║\n" +
+                   "╚═══════════════════════════════════════════════════════╝";            
+            Console.WriteLine(text);
+
+            return text;
+        }
+        static void Capcalera()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                    AGENDA DE CONTACTES                         ");
+            Console.WriteLine("\r");
+            Console.WriteLine("\r");
+            return;
+        }
+        static void Capcalera1()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                    LLISTA DE CONTACTES                         ");
+            Console.WriteLine("\r");
+            Console.WriteLine("\r");
+            return;
+        }
+        static void Capcalera2()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                    AFEGIR CONTACTES                         ");
+            Console.WriteLine("\r");
+            Console.WriteLine("\r");
+            return;
+        }
+        static void Capcalera3()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                    ESBORRAR CONTACTES                         ");
+            Console.WriteLine("\r");
+            Console.WriteLine("\r");
+            return;
+        }
+        static void Capcalera4()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                    LLISTA DE CONTACTES ESBORRATS              ");
+            Console.WriteLine("\r");
+            Console.WriteLine("\r");
+            return;
+        }
+        static void ConsolaFons()
+        {
+            Console.WindowHeight = 30;
+            Console.WindowWidth = 100;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+
+        }
+    }
+}
